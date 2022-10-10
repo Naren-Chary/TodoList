7726100e11923b1135f39510b527567c28c2113a
Naren-Chary <nagoju.naren@gmail.com>
2022-10-10 20:43:15 +0530

    todolist funtionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { takeLast } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'TodoList';
+  @ViewChild('task') inputName:any; //refer to input box task
+  list:any[]=[]; //defining empty list
+  //method which adds task into list
+  addTask(data:string){
+    this.list.push({id:this.list.length,name:data}); //inserting id and task name into the list
+    this.inputName.nativeElement.value = ' '; //used to clear the field
+  }
+  //method which remove task from list
+  removeTask(i:number){
+    this.list=this.list.filter(item=>item.id!==i);
+    //we cannot delete an item from list.therefore we use filter
+  }
 }
